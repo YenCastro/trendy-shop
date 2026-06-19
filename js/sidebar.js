d5699af335cdd7d9fd6ca166dfdf0987eeb17fe1
NicolasPirajan <nicolasalejandro2001@gmail.com>
2026-06-19 16:15:25 -0500

    Se agregan cambios de catalogo y sidebar

--- js/sidebar.js
+++ js/sidebar.js
@@ -24,6 +24,7 @@ export class SidebarMenu {
           <button id="closeSidebar" class="sidebar__close" type="button" aria-label="Cerrar menu"><i class="bi bi-x-lg"></i></button>
         </div>
 <div class="sidebar__content">
+  <div class="sidebar__items"></div>  
   <div class="sidebar__empty">
     <div class="sidebar__empty-icon"><i class="bi bi-bag-heart"></i></div>
 
@@ -39,6 +40,8 @@ export class SidebarMenu {
     `;
   }
 
+
+
   ensureMarkup() {
     const hasSidebar = document.getElementById('sidebar');
     const hasOverlay = document.getElementById('overlay');
@@ -50,6 +53,8 @@ export class SidebarMenu {
     this.sidebar = document.getElementById('sidebar');
     this.overlay = document.getElementById('overlay');
     this.closeBtn = document.getElementById('closeSidebar');
+    this.itemsContainer = document.querySelector('.sidebar__items');
+    this.emptyState = document.querySelector(".sidebar__empty");
   }
 
   ensureStyles() {
@@ -112,70 +117,119 @@ export class SidebarMenu {
         pointer-events: auto;
       }
 
-  @keyframes float {
-    0% {
-      transform: translateY(0);
+      @keyframes float {
+        0% {
+          transform: translateY(0);
+        }
+
+        50% {
+          transform: translateY(-8px);
+        }
+
+        100% {
+          transform: translateY(0);
+        }
+      }
+      @keyframes fadeIn {
+        from {
+            opacity: 0;
+            transform: translateY(8px);
+        }
+
+        to {
+            opacity: 1;
+            transform: translateY(0);
+        }
+      }
+          .sidebar__empty-icon {
+            font-size: 3rem;
+            text-align: center;
+            animation: float 3s ease-in-out infinite;
+            filter: drop-shadow(0 8px 16px rgba(209, 122, 91, 0.15));
+    }
+        .sidebar__empty {
+          text-align: center;
+          animation: fadeIn 0.4s ease;
     }
 
-    50% {
-      transform: translateY(-8px);
+        .sidebar__empty h3 {
+          margin-top: 1rem;
+          font-size: 1.2rem;
+          font-weight: 600;
+    }
+        .sidebar__empty p {
+          opacity: 0.75;
+          line-height: 1.6;
+    }
+        .sidebar__close{
+        width: 34px;
+        height: 34px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border: none;
+        border-radius: 50%;
+        background: rgba(255, 255, 255, 0.8);
+        cursor: pointer;
+        transition: all 0.25s ease;
+    }
+      .sidebar__close {
+        font-size: 1.1rem;
+        font-weight: 300;
+    }
+        .sidebar__close:hover {
+          transform: rotate(90deg);
+          background: rgba(255, 255, 255, 0.95);
+          box-shadow: 0 6px 16px rgba(209, 122, 91, 0.15);
+    }
+    .sidebar__item {
+      display: flex;
+      align-items: center;
+      gap: 12px;
+      padding: 12px;
+      margin-bottom: 10px;
+      border-radius: 12px;
+      background: rgba(255,255,255,0.7);
     }
 
-    100% {
-      transform: translateY(0);
-    }
+      .sidebar__item img {
+        width: 60px;
+        height: 60px;
+        object-fit: cover;
+        border-radius: 10px;
+      }
+
+      .sidebar__item h4 {
+        margin: 0;
+        font-size: 0.9rem;
+      }
+
+      .sidebar__item p {
+        margin: 4px 0 0;
+        font-weight: 600;
+        color: #e4764e;
+      }
+      .sidebar__info {
+    flex: 1;
   }
-  @keyframes fadeIn {
-    from {
-        opacity: 0;
-        transform: translateY(8px);
-    }
 
-    to {
-        opacity: 1;
-        transform: translateY(0);
-    }
-  }
-      .sidebar__empty-icon {
-        font-size: 3rem;
-        text-align: center;
-        animation: float 3s ease-in-out infinite;
-        filter: drop-shadow(0 8px 16px rgba(209, 122, 91, 0.15));
-}
-    .sidebar__empty {
-      text-align: center;
-      animation: fadeIn 0.4s ease;
+  .sidebar__remove {
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: none;
+  background: rgba(255,255,255,0.7);
+  cursor: pointer;
+  transition: all .25s ease;
 }
 
-    .sidebar__empty h3 {
-      margin-top: 1rem;
-      font-size: 1.2rem;
-      font-weight: 600;
-}
-    .sidebar__empty p {
-      opacity: 0.75;
-      line-height: 1.6;
-}
-    .sidebar__close{
-    width: 34px;
-    height: 34px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    border: none;
-    border-radius: 50%;
-    background: rgba(255, 255, 255, 0.8);
-    cursor: pointer;
-    transition: all 0.25s ease;
-}
-  .sidebar__close {
-    font-size: 1.1rem;
-    font-weight: 300;
-}
-    .sidebar__close:hover {
-      transform: rotate(90deg);
-      background: rgba(255, 255, 255, 0.95);
-      box-shadow: 0 6px 16px rgba(209, 122, 91, 0.15);
+  .sidebar__remove:hover {
+  transform: scale(1.1);
+  color: #e4764e;
+  background: white;
 }
     `;
 
@@ -200,10 +254,76 @@ export class SidebarMenu {
   }
 
   open() {
+    this.renderCart();
+
     this.sidebar.classList.add('is-open');
     this.sidebar.setAttribute('aria-hidden', 'false');
     this.overlay.classList.add('is-active');
+    this.emptyState.style.display = "block";
+    this.emptyState.style.display = "none";
   }
+
+renderCart() {
+  const carrito =
+    JSON.parse(localStorage.getItem("carrito")) || [];
+
+  console.log(carrito);
+
+  const emptyState =
+    document.querySelector(".sidebar__empty");
+
+  if (carrito.length === 0) {
+    emptyState.style.display = "block";
+    this.itemsContainer.innerHTML = "";
+    return;
+  }
+
+  emptyState.style.display = "none";
+
+    this.itemsContainer.innerHTML = carrito
+    .map((producto) => `
+      <div class="sidebar__item">
+        <img
+          src="${producto.thumbnail}"
+          alt="${producto.title}"
+        >
+
+        <div class="sidebar__info">
+          <h4>${producto.title}</h4>
+          <p>$${producto.price}</p>
+        </div>
+
+        <button
+          class="sidebar__remove"
+          data-id="${producto.id}"
+        >
+          <i class="bi bi-trash"></i>
+        </button>
+      </div>
+    `)
+    .join("");
+    const botonesEliminar =
+  document.querySelectorAll(".sidebar__remove");
+
+botonesEliminar.forEach((boton) => {
+  boton.addEventListener("click", () => {
+    const id = Number(boton.dataset.id);
+
+    const nuevoCarrito = carrito.filter(
+      (producto) => producto.id !== id
+    );
+
+    localStorage.setItem(
+      "carrito",
+      JSON.stringify(nuevoCarrito)
+    );
+
+    this.renderCart();
+  });
+});
+}
+
+
 
   close() {
     this.sidebar.classList.remove('is-open');
